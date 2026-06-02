67866d5d8539cb527e19f7a42b6b673d8415f25e
Tiago Brandao Nogueira <tb6865@gmail.com>
2026-06-02 14:24:20 -0300

    Criei a estrutura de menus do sistema de controle de estoque

---
+++ bibliotecas/menuEscolhas.c
@@ -0,0 +1,51 @@
+// MODULARIZAÇÃO DOS MENUS UTILIZADOS NO PROJETO DE GERENCIAMENTO DE ESTOQUE DE PRODUTOS
+#include <stdio.h>
+int menuGeral(){
+    int opcao;
+    printf("Menu Geral:\n");
+    printf("1. Gerenciamento de Produtos\n");
+    printf("2. Cadastro de Clientes\n");
+    printf("3. Registro de Vendas\n");
+    printf("0. Sair\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcao);
+    return opcao;
+}
+int menuProdutos(){
+    int opcaoProdutos;
+    printf("Menu de Produtos:\n");
+    printf("1. Adicionar Produto\n");
+    printf("2. Remover Produto\n");
+    printf("3. Listar Produtos\n");
+    printf("4. Atualizar Estoque\n");
+    printf("5. Buscar Produtos\n");
+    printf("6. Calcular Valor do Estoque\n");
+    printf("7. Relatório de Estoque Baixo\n");
+    printf("0. Voltar ao Menu Geral\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcaoProdutos);
+    return opcaoProdutos;
+}
+
+int menuClientes(){
+    int opcaoClientes;
+    printf("Menu de Clientes:\n");
+    printf("1. Adicionar Cliente\n");
+    printf("2. Remover Cliente\n");
+    printf("3. Listar Clientes\n");
+    printf("0. Voltar ao Menu Geral\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcaoClientes);
+    return opcaoClientes;
+}
+
+int menuVendas(){
+    int opcaoVendas;
+    printf("Menu de Vendas:\n");
+    printf("1. Registrar Venda\n");
+    printf("2. Listar Vendas\n");
+    printf("0. Voltar ao Menu Geral\n");
+    printf("Escolha uma opção: ");
+    scanf("%d", &opcaoVendas);
+    return opcaoVendas;
+}
---
+++ main.c
@@ -0,0 +1,35 @@
+// CONTROLE DE ESTOQUE DE PRODUTOS
+// 1. Módulo de Produtos: Gerenciamento do catálogo de produtos e níveis de
+// estoque.
+// 2. Módulo de Clientes: Cadastro de clientes.
+// 3. Módulo de Vendas: Registro de vendas e inventário.
+// REANAN CESAR MICHELON E TIAGO BRANDÃO NOGUEIRA 
+// JUNHO DE 2026
+#include <stdio.h>
+#include "bibliotecas/menuEscolhas.c"
+
+int main(){
+    int opcao = menuGeral(); // Chama o menu geral e armazena a opção escolhida pelo usuário
+    if (opcao == 1)
+    {
+        menuProdutos(); // Chama o menu de produtos se a opção escolhida for 1
+    }
+    else if (opcao == 2)
+    {
+        menuClientes(); // Chama o menu de clientes se a opção escolhida for 2
+    }
+    else if (opcao == 3)
+    {
+        menuVendas(); // Chama o menu de vendas se a opção escolhida for 3
+    }
+    else if (opcao == 0)
+    {
+        printf("Saindo do programa...\n"); // Exibe uma mensagem de saída se a opção escolhida for 0
+    }
+    else
+    {
+        printf("Opção inválida. Por favor, escolha uma opção válida.\n"); // Exibe uma mensagem de erro se a opção escolhida for inválida
+    }
+    
+    return 0;
+}
