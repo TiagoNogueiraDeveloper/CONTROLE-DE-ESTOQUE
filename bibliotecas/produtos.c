bef7708d90ff5bf1b88969f485a0ad329a258224
RenanMichelon <renancmichelon@gmail.com>
2026-06-10 11:46:16 -0300

    Desenvolvimento inicial:  produtos
    Criei as funções que serão usadas na biblioteca de produtos.

---
+++ bibliotecas/produtos.c
@@ -0,0 +1,19 @@
+//FUNÇÕES PARA O FUNCIONAMENTO DA  GESTÃO DE PRODUTOS 
+#include <stdio.h>
+#include <ctype.h>
+#include <string.h>
+#include <stdlib.h>
+
+int criarProduto(){}
+
+int excluirProduto(){}
+
+int listarProduto(){}
+
+int atualizarEstoque(){}
+
+int buscaProduto(){}
+
+int valorTotalEstoque(){}
+
+int alertaEstoqueBaico(){}
